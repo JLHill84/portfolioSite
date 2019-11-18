be3d363e328cabcef7990e193bf6527fb15e67e8
github username <github email address>
2019-11-18 08:31:34 -0600

    first commit

---
+++ build/precache-manifest.fdd26ca43d9703450d2fc4cec2f66778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371dd6ab2dbd6f2bd46d56b9803b8e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "660bca7e6fa1e83a9501",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0775148ff0d4f789613b",
+    "url": "/static/js/2.871c6842.chunk.js"
+  },
+  {
+    "revision": "660bca7e6fa1e83a9501",
+    "url": "/static/js/main.f4eca54c.chunk.js"
+  },
+  {
+    "revision": "42964883d16cee0e6abb",
+    "url": "/static/js/runtime-main.0f942cfb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
